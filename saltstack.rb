094ab3aa6c2b2f40b9eaf9a4c2647c7fe4ca8f0a
David Baumgold <david@davidbaumgold.com>
2012-11-24 08:09:36 -0500

    Use homebrew python

--- saltstack.rb
+++ saltstack.rb
@@ -5,15 +5,15 @@ class Saltstack < Formula
   url 'https://github.com/downloads/saltstack/salt/salt-0.10.5.tar.gz'
   sha1 '42796c7299e0000c250af2b3164fa77ef4f5e460'
 
+  depends_on 'python'
   depends_on 'zeromq'
   depends_on 'swig' => :build
 
   def install
     # Add folders to path (Superenv removes these)
-    # TODO: Move away from system virtualenv and use homebrew python
     ENV.append "PATH", "/usr/local/bin", ":"
     ENV.append "PATH", ( HOMEBREW_PREFIX / 'bin'), ":"
-    system "virtualenv", "#{prefix}/salt.venv"
+    system "#{HOMEBREW_PREFIX}/share/python/virtualenv", "#{prefix}/salt.venv"
     system "#{prefix}/salt.venv/bin/pip", "install", "-r", "requirements.txt"
     system "#{prefix}/salt.venv/bin/python", "setup.py", "install"
     bin.install_symlink "#{prefix}/salt.venv/bin/salt"
