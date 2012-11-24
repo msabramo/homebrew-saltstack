8148472ab61d665c4d9865ad42abf79fd635ed34
David Baumgold <david@davidbaumgold.com>
2012-11-24 08:04:35 -0500

    Updated salt to 0.10.5

--- saltstack.rb
+++ saltstack.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Saltstack < Formula
   homepage 'http://saltstack.org/'
-  url 'https://github.com/downloads/saltstack/salt/salt-0.10.3.tar.gz'
-  sha1 '01d95378dc00c6586ea2c25ccf79a2b3a9cf45c6'
+  url 'https://github.com/downloads/saltstack/salt/salt-0.10.5.tar.gz'
+  sha1 '42796c7299e0000c250af2b3164fa77ef4f5e460'
 
   depends_on 'zeromq'
   depends_on 'swig' => :build
