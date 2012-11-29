23b8e85086134339fc0c61859ae61ac1b5d92030
David Baumgold <david@davidbaumgold.com>
2012-11-28 23:49:34 -0500

    Remove virtualenv dependency
    Require Python modules using the depends_on => :python interface

--- saltstack.rb
+++ saltstack.rb
@@ -5,9 +5,15 @@ class Saltstack < Formula
   url 'https://github.com/downloads/saltstack/salt/salt-0.10.5.tar.gz'
   sha1 '42796c7299e0000c250af2b3164fa77ef4f5e460'
 
+  depends_on 'swig' => :build
   depends_on 'python'
   depends_on 'zeromq'
-  depends_on 'swig' => :build
+  depends_on 'jinja2' => :python
+  depends_on 'M2Crypto' => :python
+  depends_on LanguageModuleDependency.new :python, 'PyCrypto', 'Crypto'
+  depends_on LanguageModuleDependency.new :python, 'pyzmq', 'zmq'
+  depends_on LanguageModuleDependency.new :python, 'PyYAML', 'yaml'
+  depends_on LanguageModuleDependency.new :python, 'msgpack-python', 'msgpack'
 
   def patches
     # set salt config dir to live under Homebrew's etc dir
@@ -15,21 +21,23 @@ class Saltstack < Formula
   end
 
   def install
-    # Add folders to path (Superenv removes these)
-    ENV.append "PATH", "/usr/local/bin", ":"
-    ENV.append "PATH", ( HOMEBREW_PREFIX / 'bin'), ":"
-    system "#{HOMEBREW_PREFIX}/share/python/virtualenv", "#{prefix}/salt.venv"
-    system "#{prefix}/salt.venv/bin/pip", "install", "-r", "requirements.txt"
-    system "#{prefix}/salt.venv/bin/python", "setup.py", "install"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-call"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-cp"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-key"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-master"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-minion"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-run"
-    bin.install_symlink "#{prefix}/salt.venv/bin/salt-syndic"
-    (etc+'salt').mkpath
+    system "python", "setup.py", "install"
+  end
+
+  def uninstall  # currently nonfunctional
+    super
+    system "pip", "uninstall", "salt"
+  end
+
+  def scripts_folder
+    HOMEBREW_PREFIX/"share/python"
+  end
+
+  def caveats
+    <<-EOS.undent
+      To run the `salt` command, you'll need to add Python's script directory to your PATH:
+        #{scripts_folder}
+    EOS
   end
 
   def test
